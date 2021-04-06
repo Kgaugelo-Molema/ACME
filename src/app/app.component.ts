d956475828efd5a06119b2c0dfcdcdb8772b246a
Kgaugelo Molema <kmolema@gmail.com>
2021-04-06 13:22:50 +0200

    Subscribe to accounts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'acme';
 
   columnDefs = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price'}
+    { field: 'account_number', sortable: true, filter: true },
+    { field: 'account_type', sortable: true, filter: true },
+    { field: 'balance', sortable: true, filter: true }
   ];
 
-rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  rowData: any[];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    const self = this;
+    let service = this.http.get('http://localhost:8080/api/accounts');
+    service.subscribe(data => {
+      self.rowData = data as any[];
+    });
+  }
 }
